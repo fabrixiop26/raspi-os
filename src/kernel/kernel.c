eae37b794ffba6b17c12ff0b6b4aa18e2e6c35f6
fabrixiop26 <fabriciopuche@hotmail.com>
2021-03-15 17:21:01 -0500

    multicore funcionando exepto en archivo binario .img

--- src/kernel/kernel.c
+++ src/kernel/kernel.c
@@ -1,11 +1,42 @@
 #include "drivers/uart.h"
 
-void kernel_main(void)
-{
-    uart_init();
-    uart_send_string("Hello, world!\r\n");
+//variable global para controlar el procesador
+static unsigned int semaphore = 0;
 
-    while (1) {
-        uart_send(uart_recv());
+//get processor id as 'char' from 'x0'
+void kernel_main(char proc_id)
+{
+    // Wait for CPU #0 to finish initalizacion of uart
+    while (proc_id != semaphore)
+    {
+    }
+    // only the master (processor id = 0) initialize uart
+    if (proc_id == 0)
+    {
+        uart_init();
+        //\r mueve el "cursor al principio de la linea"
+        uart_send_string("Hello, world!\r\n");
+    }
+
+    uart_send_string("Processor #");
+    uart_send(proc_id + '0'); //Se contatena 0 para volverlo ascii
+    uart_send_string(" initialized");
+    uart_send_string("\r\n");
+
+    //Aumento la variable de control para que la siguiente cpu salga del bucle
+    semaphore++;
+
+    //Solo la cpu 0 tiene permitido hacer echo, los demas terminan
+    if (proc_id == 0)
+    {
+        // Wait for everyone else to finish
+        while (semaphore != 4)
+        {
+        }
+
+        while (1)
+        {
+            uart_send(uart_recv());
+        }
     }
 }
